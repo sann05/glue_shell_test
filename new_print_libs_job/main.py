b95b623c18576ae404623611d03732fb53fc3b3e
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2023-05-30 12:44:52 +0200

    Changed params

--- new_print_libs_job/main.py
+++ new_print_libs_job/main.py
@@ -7,11 +7,13 @@ if __name__ == '__main__':
     help('modules')
     print(searchads_api.__name__)
     print(pyeloqua.__name__)
-
+    sys_paths = sys.path
     print(f"os.getcwd()={os.getcwd()}")
     print(f"os.listdir('.')={os.listdir('.')}")
 
-    print(f"sys.path={sys.path}")
+    print(f"sys.path={sys_paths}")
+    for p in sys_paths:
+        print(f"os.listdir('{p}')={os.listdir(p)}")
     try:
         from mymodule import module
 
