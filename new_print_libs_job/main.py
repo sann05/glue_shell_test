10acebfd8b697a2f13ead28db9f6c0d3dcc452ad
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2023-05-30 12:16:02 +0200

    Changed params

--- new_print_libs_job/main.py
+++ new_print_libs_job/main.py
@@ -1,9 +1,11 @@
 import sys
 import searchads_api
 import pyeloqua
+from mymodule import module
 
 
 if __name__ == '__main__':
     help('modules')
     print(searchads_api.__name__)
     print(pyeloqua.__name__)
+    module.hello()
