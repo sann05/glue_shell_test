732854c5ebe978c3c285db52473307d15e92b174
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2023-05-30 11:25:36 +0200

    Added different modules

--- new_print_libs_job/main.py
+++ new_print_libs_job/main.py
@@ -1,8 +1,8 @@
 import sys
-import inner_file
-from ..local_file import NAME as local_name
+from inner_module import inner_file
+import local_file
 
 if __name__ == '__main__':
     help('modules')
     print(inner_file.NAME)
-    print(local_name)
+    print(local_file.NAME)
