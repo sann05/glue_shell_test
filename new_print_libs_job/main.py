19c6964ac6f8826b57f0637f3a66f740a2843af5
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2023-05-30 12:24:13 +0200

    Changed params

--- new_print_libs_job/main.py
+++ new_print_libs_job/main.py
@@ -1,11 +1,22 @@
 import sys
 import searchads_api
 import pyeloqua
-from mymodule import module
-
 
 if __name__ == '__main__':
     help('modules')
     print(searchads_api.__name__)
     print(pyeloqua.__name__)
-    module.hello()
+    try:
+        from mymodule import module
+
+        module.hello()
+        print("Found module")
+    except ImportError:
+        print("Cannot find module")
+    try:
+        import module
+
+        module.hello()
+        print("Found content")
+    except ImportError:
+        print("Cannot find content")
