a2e1a8ed9887bd66caa7a5a3a1bf1a0213f5c9a8
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2023-05-30 11:38:09 +0200

    Added 2 external libs

--- new_print_libs_job/main.py
+++ new_print_libs_job/main.py
@@ -1,8 +1,12 @@
 import sys
 from inner_module import inner_file
+import searchads_api
+import pyeloqua
 import local_file
 
 if __name__ == '__main__':
     help('modules')
     print(inner_file.NAME)
     print(local_file.NAME)
+    print(searchads_api.__name__)
+    print(pyeloqua.__name__)
